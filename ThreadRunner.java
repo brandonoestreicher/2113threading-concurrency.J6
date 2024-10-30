c1ac129932b219cb20650e73d4b89ebbc4ef4189
brandonoestreicher <brandonoestreicher@gmail.com>
2024-10-30 00:28:01 -0400

    Added ThreadRunner.java

---
+++ ThreadRunner.java
@@ -0,0 +1,33 @@
+/* ThreadRunner.java */
+public class ThreadRunner {
+
+    private static boolean statusFlag = false;
+
+    private static class SimpleThreadOne extends Thread {
+        public void run() {
+            for (int i = 1; i <= 2000; i++){
+                System.out.println("Simple thread one counter - " + i);
+            }
+            // Change the status flag.
+            statusFlag = true;
+            System.out.println("Status flag changed to true in simple thread one.");
+        }
+    }
+
+    private static class SimpleThreadTwo extends Thread {
+        public void run() {
+            int waitCounter = 1;
+            while (!statusFlag) {
+                waitCounter++;
+            }
+            System.out.println("Start simple thread two processing " + waitCounter);
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleThreadOne simpleThreadOne = new SimpleThreadOne();
+        simpleThreadOne.start();
+        SimpleThreadTwo simpleThreadTwo = new SimpleThreadTwo();
+        simpleThreadTwo.start();
+    }
+}
