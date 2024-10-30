61975e9195a8e2a7436221ab8a589353fb017975
brandonoestreicher <brandonoestreicher@gmail.com>
2024-10-30 00:03:30 -0400

    Modified AnimalFootRace.java according to Question 9

--- AnimalFootRace.java
+++ AnimalFootRace.java
@@ -3,16 +3,13 @@ public class AnimalFootRace {
     public static void main(String[] args) {
         Thread tortoiseThread = new AnimalRacerThread("Tortoise", 5);
         Thread hareThread = new AnimalRacerThread("Hare", 20);
+        Thread humanThread = new AnimalRacerThread("Human", 35);
         Thread cheetahThread = new AnimalRacerThread("Cheetah", 50);
 
         System.out.println("On your marks, get set, go!");
-        hareThread.start();
-        try {
-            hareThread.join();
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted while joining");
-        }
         tortoiseThread.start();
+        hareThread.start();
+        humanThread.start();
         cheetahThread.start();
     }
 }
